1ee302c528a62fa6057aa07a3117791f921d1186
seunghwan <wxtq22@naver.com>
2025-03-18 11:11:41 +0900

    .

--- sbb/src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -10,8 +10,8 @@ import lombok.Setter;
 public class QuestionForm {
     @NotEmpty(message="제목은 필수항목입니다.") // 제목이 비어있으면 오류 메시지 출력
     @Size(max=200) // 최대 길이 200자로 제한
-    private String subject;
+    private String subject; // 제목
 
     @NotEmpty(message="내용은 필수항목입니다.") // 내용이 비어있으면 오류 메시지 출력
-    private String content;
+    private String content; // 내용
 }

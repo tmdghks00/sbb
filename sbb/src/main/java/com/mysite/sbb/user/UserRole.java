6a1c88a640a8c7e847a148b1b8c84626a03998ff
seunghwan <wxtq22@naver.com>
2025-03-20 09:19:08 +0900

    .

--- sbb/src/main/java/com/mysite/sbb/user/UserRole.java
+++ sbb/src/main/java/com/mysite/sbb/user/UserRole.java
@@ -5,11 +5,12 @@ import lombok.Getter;
 @Getter
 public enum UserRole {
     ADMIN("ROLE_ADMIN"), // 관리자 권한
-    USER("ROLE_USER"); // 일반 사용자 권한
+    USER("ROLE_USER");   // 일반 사용자 권한
 
+    // 생성자를 통해 enum 상수에 지정된 값(value)을 설정
     UserRole(String value) {
         this.value = value;
     }
 
-    private String value;
+    private String value; // 권한 값을 저장하는 변수
 }

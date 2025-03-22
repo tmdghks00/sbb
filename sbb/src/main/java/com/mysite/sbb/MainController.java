2d1c8e4500035c0beb90da3f295dc7bb717d4c38
SeungHwan Oh <wxtq22@naver.com>
2025-03-22 19:14:03 +0900

    .

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // 스프링 MVC 컨트롤러 등록
 public class MainController {
 
+/*    만약 @ResponseBody 애너테이션을 생략한다면 스프링 부트는
+    'index'라는 문자열을 리턴하는 대신 index라는 이름의 템플릿 파일을 찾게됨 */
     @GetMapping("/sbb") // "/sbb" 요청 처리
-    @ResponseBody // 응답 본문으로 문자열 반환
+    @ResponseBody //  URL 요청에 대한 응답으로 문자열 반환
     public String index() {
         return "안녕하세요 sbb에 오신것을 환영합니다."; // 메시지 출력
     }
